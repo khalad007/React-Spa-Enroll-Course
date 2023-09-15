2c09c231620b5e7c7a430153678e1c19dddffe42
khalad007 <www.arifhossain49919@gmail.com>
2023-09-15 04:44:49 -0700

    Created Fake Data & Load Data

---
+++ src/component/Cards/Cards.jsx
@@ -0,0 +1,20 @@
+import { data } from "autoprefixer";
+import { useEffect, useState } from "react";
+
+const Cards = () => {
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setCards(data))
+    }, [])
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Cards;
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-6xl text-center'>Course Registration</h1>
+            
+        </div>
+
+    );
+};
+
+export default Header;
